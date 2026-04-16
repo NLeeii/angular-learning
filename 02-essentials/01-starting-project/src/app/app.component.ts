4fc959fa5f2b964ad82c54ea411e564096abc4c2
nicole_lee <niniabc920405@gmail.com>
2026-04-16 17:41:25 +0800

    到課程2-16

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // 在 HTML 中使用的標籤名稱
   standalone: true,
-  imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  imports: [HeaderComponent],
+  templateUrl: './app.component.html', // 定義元件的HTML
+  styleUrl: './app.component.css', // 定義元件的CSS。style 寫陣列樣式;styleUrls 指定外部 css 檔案路徑陣列
 })
 export class AppComponent {}
