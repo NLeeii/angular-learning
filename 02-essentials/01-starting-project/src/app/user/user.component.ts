daabcf66dc3a965ea5d017cde4df973898a75cbf
nicole_lee <niniabc920405@gmail.com>
2026-04-20 16:39:52 +0800

    signal inputs

--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, input, computed } from '@angular/core';
 // import { DUMMY_USERS } from '../dummy-users';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,20 +11,32 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.scss'
 })
 export class UserComponent {
+
+  // @Input()裝飾器 vs input()函式
+
   // @Input()標記該屬性可從外部設置，因為是接收”外部”來的值，所以需要先訂定”接收的型別”
   // 透過"!"告訴TypeScript這肯定會被設置為某個值
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
   @Input() id!: string;
 
-  get imagePath(): string {
-    return 'assets/users/' + this.avatar;
-  }
-
   // 需要將@Input()的值進行初始化
   // constructor() {
   //   this.avatar = '';
   // }
+  
+  get imagePath(): string {
+    return 'assets/users/' + this.avatar;
+  }
+
+  // signal inputs ========================================================
+  // avatar = input<string>();
+  // avatar = input.required<string>();
+
+  // 配合signal使用的computed
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar;
+  // })
 
   onSelectUser() {}
 }
