8bb63918d05962e576f90862fc651ad78dd0ac30
nicole_lee <niniabc920405@gmail.com>
2026-04-20 17:34:48 +0800

    output區域準備

--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -12,9 +12,11 @@ import { Component, Input, input, computed } from '@angular/core';
 })
 export class UserComponent {
 
+  // 輸入_input =============================================================================
   // @Input()裝飾器 vs input()函式
 
   // @Input()標記該屬性可從外部設置，因為是接收”外部”來的值，所以需要先訂定”接收的型別”
+  // @Input() "接受輸入" 這個特性及概念，允許重用具有不同數據(由外部輸入)的component
   // 透過"!"告訴TypeScript這肯定會被設置為某個值
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string;
@@ -37,6 +39,8 @@ export class UserComponent {
   // imagePath = computed(() => {
   //   return 'assets/users/' + this.avatar;
   // })
+
+  // 輸出_output =============================================================================
 
   onSelectUser() {}
 }
