3f1120b7e18dca8c22de2d3eb50779dc9007a18a
nicole_lee <niniabc920405@gmail.com>
2026-04-20 15:15:36 +0800

    1. 更改樣式為scss 2. signal演示(user.component)

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -7,6 +7,6 @@ import { UserComponent } from './user/user.component';
   standalone: true,
   imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html', // 定義元件的HTML
-  styleUrl: './app.component.css', // 定義元件的CSS。style 寫陣列樣式;styleUrls 指定外部 css 檔案路徑陣列
+  styleUrl: './app.component.scss', // 定義元件的CSS。style 寫陣列樣式;styleUrls 指定外部 css 檔案路徑陣列
 })
 export class AppComponent {}
--- 02-essentials/01-starting-project/src/app/user/user.component.ts
+++ 02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -8,17 +8,21 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.scss'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+
 
   // getter 本質上是一個方法，也就是class中的一個函數。可以像property一樣使用，所以不需要顯示調用和執行，它的目的就是生成和返回一個新值。補充:因為getter可以像property一樣使用，因此在使用時候面不用加"執行用"的()。
-  get imagePath() { 
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  // get imagePath() { 
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
 
   onSelectUser() {
-    alert('click!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
