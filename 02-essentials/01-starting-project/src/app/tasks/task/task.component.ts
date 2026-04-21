12fce20d842b006dac3ebab0dde9191c0f795b98
nicole_lee <niniabc920405@gmail.com>
2026-04-21 17:13:48 +0800

    個別用戶任務篩選

---
+++ 02-essentials/01-starting-project/src/app/tasks/task/task.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.scss'
+})
+export class TaskComponent {
+  @Input({required: true}) title!:string;
+  @Input({required: true}) summary!:string;
+  @Input({required: true}) dueDate!:string;
+
+}
