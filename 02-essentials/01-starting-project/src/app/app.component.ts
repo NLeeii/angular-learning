9336f2e08fa50365a1a20902af11d7b452882e8a
nicole_lee <niniabc920405@gmail.com>
2026-04-21 16:07:20 +0800

    1. @for & @if - 動態呈現列表 2. *ngFor 、 *ngIf & ng-template

--- 02-essentials/01-starting-project/src/app/app.component.ts
+++ 02-essentials/01-starting-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
@@ -8,14 +9,14 @@ import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root', // 在 HTML 中使用的標籤名稱
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TasksComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent, NgFor, NgIf],
   templateUrl: './app.component.html', // 定義元件的HTML
   styleUrl: './app.component.scss', // 定義元件的CSS。style 寫陣列樣式;styleUrls 指定外部 css 檔案路徑陣列
 })
 
 export class AppComponent {
   users = DUMMY_USERS;
-  selectedUserId = 'u1'; 
+  selectedUserId?: string; 
 
   get selectedUser() {
     return this.users.find(user => user.id === this.selectedUserId);
