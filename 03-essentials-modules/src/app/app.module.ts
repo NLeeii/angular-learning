d8cd4ef4f6e3b24bf0127da9abb1314153c6512d
nicole_lee <niniabc920405@gmail.com>
2026-04-24 10:41:27 +0800

    (第三章完結)-Module-based App

--- 03-essentials-modules/src/app/app.module.ts
+++ 03-essentials-modules/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent],
-  bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, SharedModule] // for standalone components / other modules
+  declarations: [AppComponent, HeaderComponent, UserComponent],
+  bootstrap: [AppComponent], // 只會出現在最根部的 module
+  imports: [BrowserModule, SharedModule, TasksModule], // for standalone components / other modules
+  // BrowserModule：Angular 內建部門，提供瀏覽器渲染所需的基本功能（如 ngIf, ngFor）
+  // FormsModule：Angular 內建部門，提供表單處理功能。
 })
 
 export class AppModule {}
