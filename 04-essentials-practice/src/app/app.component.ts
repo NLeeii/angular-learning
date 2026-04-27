d3ec4deb873285d789f63c88b2222f32f2e314c4
nicole_lee <niniabc920405@gmail.com>
2026-04-27 10:32:32 +0800

    表格功能/顯示完成

--- 04-essentials-practice/src/app/app.component.ts
+++ 04-essentials-practice/src/app/app.component.ts
@@ -1,11 +1,55 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
+import { type InvestmentInput } from './investment-input.model';
+import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  resultsData?: {
+    year: number,
+    interest: number,
+    valueEndOfYear: number,
+    annualInvestment: number,
+    totalInterest: number,
+    totalAmountInvested: number,
+  }[];
+
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, duration, expectedReturn, annualInvestment } = data; // 解構賦值 - Destructuring Assignment，快速把物件 (Object) 裡面的屬性「拆解」出來，並宣告成獨立變數。
+    // 傳統寫法:
+    // const initialInvestment = data.initialInvestment; const duration = data.duration; ...
+    
+
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    // return annualData;
+    // console.log(annualData);
+
+    this.resultsData = annualData;
+    
+  }
+
+}
--- 04-essentials-practice/src/app/user-input/user-input.component.ts
+++ 04-essentials-practice/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import  type { InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -8,14 +9,25 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
+
 export class UserInputComponent {
+
+  // @Output() userInput = new EventEmitter();
+  @Output() calculate = new EventEmitter<InvestmentInput>();
+
   enteredInit = '0';
   enteredAnnual = '0';
   enteredExpected = '5';
-  enteredDuration = '5';
+  enteredDuration = '10';
 
   onSubmit() {
-    console.log(this.enteredInit);
+    this.calculate.emit({
+      initialInvestment: +this.enteredInit, // 加上 "+" 轉成 number type
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpected,
+      annualInvestment: +this.enteredAnnual
+
+    })
   }
 
 }
