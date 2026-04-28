848a8d182a4546a467a1b960e3c3a55851481a0e
nicole_lee <niniabc920405@gmail.com>
2026-04-28 14:21:27 +0800

    1. Adding Forms to Components 2. 客製化component-透過attribute selector擴展內置component(像是button)(預設使用的是element selector)-省去不必要的DOM元素

---
+++ 06-cmp-deep-dive/src/app/shared/button/button.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // css attribute selector
+  selector: 'button[appButton]',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+}
