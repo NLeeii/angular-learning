a297f02d1344bca1b278c7714daab1bb207da4af
nicole_lee <niniabc920405@gmail.com>
2026-04-28 17:28:51 +0800

    (修改中) 1. Encapsulation - 封裝 2. Host Elements 3. :host Selector 4. host property

--- 06-cmp-deep-dive/src/app/shared/control/control.component.ts
+++ 06-cmp-deep-dive/src/app/shared/control/control.component.ts
@@ -1,11 +1,19 @@
-import { Component, input, Input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  // encapsulation - 封裝
+  // 禁用樣式作用域，禁用封裝
+  encapsulation:ViewEncapsulation.None,
+  // host
+  // 定義作為 host element 上的property
+  host: {
+    class: 'control'
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
