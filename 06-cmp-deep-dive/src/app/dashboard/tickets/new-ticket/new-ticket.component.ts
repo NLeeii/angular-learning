d51f170a4e6806ffd857f411486cdd6ec90fc19d
nicole_lee <niniabc920405@gmail.com>
2026-05-05 10:46:33 +0800

    清空表單內容 -Template Variables - reset

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -12,9 +12,11 @@ import { FormsModule } from '@angular/forms';
 })
 export class NewTicketComponent {
   // Template Variables - Angular提供
-  onSubmit(title: string, ticketText: string) {
+  onSubmit(title: string, ticketText: string, form: HTMLFormElement) {
     console.log(title);
     console.log(ticketText);
+    // 送出後重置/清空表單欄位
+    form.reset();
   }
 
   // ngModel 用法
