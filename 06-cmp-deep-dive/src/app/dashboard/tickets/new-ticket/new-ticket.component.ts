d1963a891097fc1088d9c3de6410687156a8960a
nicole_lee <niniabc920405@gmail.com>
2026-05-05 10:39:44 +0800

    1. Form Submissions - ngSubmit( ) 2. 提取送出表單的輸入值-Template Variables 3. Template Variables & Component Instances

--- 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // Template Variables - Angular提供
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+  }
 
+  // ngModel 用法
+  // @Input() title!: string;
+  // onSubmit() {
+  //   console.log(this.title);
+  // }
 }
