8075f431017ebde3c7aa7b0056b4c21cf4902efc
nicole_lee <niniabc920405@gmail.com>
2026-05-06 14:35:09 +0800

    1. @for 語句進階功能 - Template For Loops - Deep Dive 2. Revisiting Inputs & Signals 3. Updating Signal Values 4. Cross-Component Communication & State Management

--- 06-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/ticket/ticket.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, input, Output, output, signal } from '@angular/core';
+import { Ticket } from '../ticket.model';
 
 @Component({
   selector: 'app-ticket',
@@ -8,5 +9,22 @@ import { Component } from '@angular/core';
   styleUrl: './ticket.component.css'
 })
 export class TicketComponent {
+  data = input.required<Ticket>();
+  detailsVisible = signal(false);
 
+  // closeTicket = output<void>();
+  @Output() closeTicket = new EventEmitter;
+
+
+  onToggleDetails() {
+    // this.detailsVisible.set(!this.detailsVisible());
+
+    // signal update method
+    // 自動將舊的signal值作為參數傳遞
+    this.detailsVisible.update((wasVisible) => !wasVisible ); 
+  }
+
+  onMarkAsCompleted() {
+    this.closeTicket.emit();
+  }
 }
--- 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ 06-cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
 import { Ticket } from './ticket.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
@@ -20,5 +21,15 @@ export class TicketsComponent {
       status: 'open'
     }
     this.tickets.push(ticket)
+  }
+
+  onCloseTicket(id: string) {
+    this.tickets = this.tickets.map((ticket) => {
+      if(ticket.id === id) {
+        return { ...ticket, status: 'close' }
+      }
+      return ticket;
+    });
+    console.log('Deleted!');
   }
 }
