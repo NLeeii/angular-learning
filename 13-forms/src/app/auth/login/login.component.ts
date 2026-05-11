dc04c76291fe5535564feb22aa95a9c08d2293d7
nicole_lee <niniabc920405@gmail.com>
2026-05-11 11:22:24 +0800

    重新開始 Reactive Forms 學習 - 從第 251 章開始
    1. 在 TypeScript 裡設置表單

--- 13-forms/src/app/auth/login/login.component.ts
+++ 13-forms/src/app/auth/login/login.component.ts
@@ -9,39 +9,51 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  // ==========================
   form = new FormGroup({
-    email: new FormControl('', {
-      validators: [Validators.email, Validators.required]
-    }),
-    password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
-    }),
+    email: new FormControl(''),
+    password: new FormControl('')
   });
 
-  // 簡單email欄位驗證
-  // get 是一個用來封裝複雜邏輯判斷的極佳工具
-  // 可比作為命名清晰、唯讀且隨時保持最新狀態的「虛擬屬性」
-  get emailIsInvalid() {
-    return (
-      this.form.controls.email.touched &&
-      this.form.controls.email.dirty &&
-      this.form.controls.email.invalid
-    );
-  }
-
-  // 簡單password欄位驗證
-  get passwordIsInvalid() {
-    return (
-      this.form.controls.password.touched &&
-      this.form.controls.password.dirty &&
-      this.form.controls.password.invalid
-    );
-  }
-
   onSubmit() {
-    console.log(this.form);
-    const enteredEmail = this.form.value.email;
-    const enteredPassword = this.form.value.password;
-    console.log(enteredEmail, '+', enteredPassword);
+    this.form.value.email
   }
+
+  // ==========================
+  // 舊的
+  // form = new FormGroup({
+  //   email: new FormControl('', {
+  //     validators: [Validators.email, Validators.required]
+  //   }),
+  //   password: new FormControl('', {
+  //     validators: [Validators.required, Validators.minLength(6)]
+  //   }),
+  // });
+
+  // // 簡單email欄位驗證
+  // // get 是一個用來封裝複雜邏輯判斷的極佳工具
+  // // 可比作為命名清晰、唯讀且隨時保持最新狀態的「虛擬屬性」
+  // get emailIsInvalid() {
+  //   return (
+  //     this.form.controls.email.touched &&
+  //     this.form.controls.email.dirty &&
+  //     this.form.controls.email.invalid
+  //   );
+  // }
+
+  // // 簡單password欄位驗證
+  // get passwordIsInvalid() {
+  //   return (
+  //     this.form.controls.password.touched &&
+  //     this.form.controls.password.dirty &&
+  //     this.form.controls.password.invalid
+  //   );
+  // }
+
+  // onSubmit() {
+  //   console.log(this.form);
+  //   const enteredEmail = this.form.value.email;
+  //   const enteredPassword = this.form.value.password;
+  //   console.log(enteredEmail, '+', enteredPassword);
+  // }
 }
