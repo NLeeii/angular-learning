0f240f4f77ea964f5bca00a22406642dd313762f
nicole_lee <niniabc920405@gmail.com>
2026-05-11 16:49:14 +0800

    註冊表單 - sign up
    1. 取得表單輸入值
    2. 清空表單
    3. 註冊&連結複雜表單

--- 13-forms/src/app/auth/signup/signup.component.ts
+++ 13-forms/src/app/auth/signup/signup.component.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css',
 })
-export class SignupComponent {}
+export class SignupComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.email, Validators.required]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    confirmPassword: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6)]
+    }),
+    firstName: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    street: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    number: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    postalCode: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    role: new FormControl<'student' | 'teacher' | 'employee' | 'founder' | 'other'>('student', {
+      validators: [Validators.required]
+    }),
+    agree: new FormControl(false, {
+      validators: [Validators.required]
+    })
+  });
+
+  onSubmit() {
+    const enteredEmail = this.form.controls.email.value
+    const enteredPassword = this.form.controls.password.value
+    console.log(enteredEmail, enteredPassword);
+    this.onClear();
+  }
+
+  onClear() {
+    this.form.reset();
+    console.log('clear');
+    
+  }
+}
